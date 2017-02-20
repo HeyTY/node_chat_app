0ab43d2e209b0be2f892ecba1fa2971bf2a99b3f
Ty Le <anhtuan.n.le@gmail.com>
2017-02-20 17:44:41 -0500

    Greet new userr and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,18 @@ socket.on("connect", function() {
 	console.log("Connnected to ChatApp!");
 });
 
+	
+	socket.on("welcomeMessage", function (message) {
+		console.log(message);
+	});
+
+
+
+
+	socket.on("newUser", function (message) {
+		console.log(message);
+	});
+
 
 	socket.on("newMessage", function (message) {
 		console.log("New Message:", message);
--- server/server.js
+++ server/server.js
@@ -20,18 +20,40 @@ io.on("connection", (socket) => {
 socket.on("createMessage", function (message){
 	console.log("Create Message", message);
 	
+
+
+	// socket.brodcast.emit // everyone but the user who join from admin "New User Joined"
+
+
+
 	// Emit to everyone on the connection
 	io.emit("newMessage", {
 		from: message.from,
 		text: message.text,
 		createdAt: new Date()
 	});
+
 });
 
+	// Broadcast new user login
+	socket.broadcast.emit("newUser",{
+		from: "Admin",
+		text: "New User has joined",
+		createdAt: new Date().toString()
+	});
 
-socket.on("disconnect", (socket) => {
-	console.log("User is offline");
-});
+
+	// Welcome User
+	socket.emit("welcomeMessage", {
+		from: "Admin",
+		text: "Welcome to ChatApp!",
+		createdAt: new Date().toString()
+	});
+
+	// Diconnect
+	socket.on("disconnect", (socket) => {
+		console.log("User is offline");
+	});
 
 });
 
